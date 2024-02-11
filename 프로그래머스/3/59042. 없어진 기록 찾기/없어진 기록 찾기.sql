8660b7306989d7de609fc301f2cd04866836a64a
SunSpike00 <83578904+SunSpike00@users.noreply.github.com>
2024-02-11 13:06:11 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -3,8 +3,10 @@
 -- ANIMAL_OUTS에서는 모든 코드가 존재함.
 -- 코드는 교집합 값을 제외한 순수 ANIMAL_INS의 값 출력
 
-select ANIMAL_OUTS.ANIMAL_ID, ANIMAL_OUTS.NAME
-from ANIMAL_OUTS
-left outer join ANIMAL_INS
-on ANIMAL_OUTS.ANIMAL_ID = ANIMAL_INS.ANIMAL_ID
-where ANIMAL_INS.ANIMAL_ID is null
+SELECT ai.ANIMAL_ID, ai.NAME
+from ANIMAL_INS as ai
+left join ANIMAL_OUTS as ao
+UNION SELECT ao.ANIMAL_ID, ao.NAME
+from ANIMAL_INS as ai
+right join ANIMAL_OUTS as ao
+where ai.ANIMAL_ID is null or ao.ANIMAL_ID is null
