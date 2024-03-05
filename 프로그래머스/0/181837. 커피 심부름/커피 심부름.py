aac1c03217ed11c5ee2a5501d2751bbaef831f55
SunSpike00 <83578904+SunSpike00@users.noreply.github.com>
2024-03-05 16:58:24 +0900

    [level 0] Title: 커피 심부름, Time: 0.00 ms, Memory: 9.91 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,12 @@
+def solution(order):
+    answer = 0
+    
+    for i in order:
+        
+        if 'americano' in i or 'anything' in i:
+            answer += 4500
+        
+        if 'cafelatte' in i:
+            answer += 5000
+    
+    return answer
